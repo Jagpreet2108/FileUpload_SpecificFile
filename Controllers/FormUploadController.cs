0a428f8c784d9c5c51c5fed03f38c7b988b7d505
Jagpreet2108 <jagpreetkaur2108@gmail.com>
2020-04-06 21:11:22 +0530

    FinalCommit

---
+++ Controllers/FormUploadController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _06April2020_2.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace _06April2020_2.Controllers
+{
+    public class FormUploadController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<FormUploadController> _logger;
+        public FormUploadController(ILogger<FormUploadController> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(FormUpload formupload)
+        {
+            string strpath = System.IO.Path.GetExtension(formupload.myform.FileName);
+            if (formupload.myform != null && (strpath == ".jpg" || strpath == ".jpeg" || strpath == ".gif" || strpath == ".png"))
+            {
+                {
+                    string filepath = $"{_env.WebRootPath}/images/{formupload.myform.FileName}";
+                    var stream = System.IO.File.Create(filepath);
+                    formupload.myform.CopyTo(stream);
+                    return Redirect("/FormUpload/Success");
+                }
+            }
+
+             return Redirect("/FormUpload/Error");                     
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/FormUpload.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _06April2020_2.Models
+{
+    public class FormUpload
+    {
+        public IFormFile myform { get; set; }
+    }
+}
